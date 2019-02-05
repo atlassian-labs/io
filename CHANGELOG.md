ad2521855e3aab1d5c2751757a35fc5ed6d76ef7
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2019-02-05 17:54:38 +0100

    JPERF-389: Clean up the Gradle project
    Update Gradle to 5.1.1 for stricter dependency scoping.
    Update gradle-release to 0.5.0 for Bitbucket fork Pipelines and future GitHub interoperability.
    Activate dependency locks.
    Remove unused dependencies.
    Bump Kotlin version.
    Stop using the deprecated Kotlin JRE stdlib in favor if JDK stdlib.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -18,6 +18,9 @@ The API consists of all public Kotlin types from `com.atlassian.performance.tool
 ## [Unreleased]
 [Unreleased]: https://bitbucket.org/atlassian/io/branches/compare/master%0Drelease-1.1.0
 
+### Fixed
+- Stop using the deprecated JRE Kotlin stdlib.
+
 ## [1.1.0] - 2018-09-12
 [1.1.0]: https://bitbucket.org/atlassian/io/branches/compare/release-1.1.0%0Drelease-1.0.0
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,31 +1,23 @@
-val kotlinVersion = "1.2.30"
+val kotlinVersion = "1.2.70"
 
 plugins {
-    kotlin("jvm").version("1.2.30")
-    id("com.atlassian.performance.tools.gradle-release").version("0.4.1")
+    kotlin("jvm").version("1.2.70")
+    id("com.atlassian.performance.tools.gradle-release").version("0.5.0")
 }
 
 configurations.all {
-    resolutionStrategy.failOnVersionConflict()
+    resolutionStrategy {
+        failOnVersionConflict()
+        activateDependencyLocking()
+    }
 }
 
 dependencies {
-    compile("org.jetbrains.kotlin:kotlin-stdlib-jre8:$kotlinVersion")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
     testCompile("junit:junit:4.12")
-    testCompile("org.hamcrest:hamcrest-library:1.3")
-    log4j(
-        "api",
-        "core",
-        "slf4j-impl"
-    ).forEach { compile(it) }
 }
 
-fun log4j(
-    vararg modules: String
-): List<String> = modules.map { module ->
-    "org.apache.logging.log4j:log4j-$module:2.10.0"
+tasks.wrapper {
+    gradleVersion = "5.1.1"
+    distributionType = Wrapper.DistributionType.ALL
 }
-
-val wrapper = tasks["wrapper"] as Wrapper
-wrapper.gradleVersion = "4.9"
-wrapper.distributionType = Wrapper.DistributionType.ALL
